d38c5c77ff31fdbe8cf716d93a49af0ef07dc94d
sophiahoelzl <sophia.hoelzl@stud.fh-campuswien.ac.at>
2023-11-28 00:05:35 +0100

    finished model

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import random
 import matplotlib.image as mpimg
 import tensorflow as tf
 from keras import Model
+import keras
+from matplotlib import pyplot as plt
 from tensorflow.keras.layers.experimental import preprocessing
 
 
@@ -109,53 +111,11 @@ def random_forest(train_images, train_labels, test_images, test_labels):
 def cnn(train_dir, test_dir):
     print("cnn: creating generator...")
     imagegen = ImageDataGenerator()
-    print("cnn: loading training data...")
-    train = imagegen.flow_from_directory("archive/train/", class_mode="categorical", shuffle=False, batch_size=128,
-                                         target_size=(256, 256))
-    print("cnn: loading testing data...")
-    test = imagegen.flow_from_directory("archive/test/", class_mode="categorical", shuffle=False, batch_size=128,
-                                        target_size=(256, 256))
+    train_data = imagegen.flow_from_directory(train_dir, class_mode="categorical", batch_size=128, target_size=(256, 256))
+    test_data = imagegen.flow_from_directory(test_dir, class_mode="categorical", batch_size=128, target_size=(256, 256), shuffle=False)
 
-    BATCH_SIZE = 128
-    train_data = tf.keras.utils.image_dataset_from_directory(train_dir, shuffle=True, image_size=(256, 256),
-                                                             labels='inferred', label_mode='categorical',
-                                                             batch_size=BATCH_SIZE)
-    test_data = tf.keras.utils.image_dataset_from_directory(test_dir, shuffle=False, image_size=(256, 256),
-                                                            labels='inferred', label_mode='categorical',
-                                                            batch_size=BATCH_SIZE)
-
-    '''data_augmentation = tf.keras.Sequential([
-        # we use the model to rescale the data
-        tf.keras.layers.Rescaling(scale=1. / 255, input_shape=(256, 256, 3)),
-        # we tried to add more preporcessing layers, however they slowed down the whole
-        # learning process to much which is why we didn't use them in the end
-        # tf.keras.layers.RandomFlip("horizontal_and_vertical"),
-        # tf.keras.layers.RandomRotation(0.5),
-    ])
-    inputs = tf.keras.layers.Input(shape=(256, 256, 3), name='input_layer')
-    sequential = data_augmentation(inputs)
-
-    conv_1 = tf.keras.layers.Conv2D(32, (3, 3), activation="relu")(sequential)
-    maxPool_1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv_1)
-    # then 64
-    conv_2 = tf.keras.layers.Conv2D(64, (3, 3), activation="relu")(maxPool_1)
-    maxPool_2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv_2)
-    # then 128
-    conv_3 = tf.keras.layers.Conv2D(128, (3, 3), activation="relu")(maxPool_2)
-    # then we flattend the all the layers
-    flat = tf.keras.layers.Flatten()(conv_3)
-    # and use some dropout (40%) to prevent overfitting
-    dropout_1 = tf.keras.layers.Dropout(0.4)(flat)
-    # then we add a hidden layer
-    dense_1 = tf.keras.layers.Dense(128, activation="relu")(dropout_1)
-    # another for the output layer
-    outputs = tf.keras.layers.Dense(num_classes, activation="softmax", name="output")(dense_1)
-
-    # then we build the model
-    model = tf.keras.Model(inputs, outputs, name="training_model")'''
     model = Sequential()
-
-    # defines the convolutional base
+    model.add(InputLayer(input_shape=(256, 256, 3)))
     # 1st conv block
     model.add(Conv2D(25, (5, 5), activation='relu', strides=(1, 1), padding='same'))
     model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
@@ -167,28 +127,32 @@ def cnn(train_dir, test_dir):
     model.add(Conv2D(70, (3, 3), activation='relu', strides=(2, 2), padding='same'))
     model.add(MaxPool2D(pool_size=(2, 2), padding='valid'))
     model.add(BatchNormalization())
-
     # ANN block
-    # to perform classification
     model.add(Flatten())
     model.add(Dense(units=100, activation='relu'))
+    model.add(Dense(units=100, activation='relu'))
     model.add(Dropout(0.25))
+    # output layer
     model.add(Dense(units=5, activation='softmax'))
 
-    # model.summary()
+    model.compile(
+        optimizer='adam',
+        loss = tf.keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy", "Recall", "Precision"],
+    )
 
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy', 'recall', 'precision'])
+    history = model.fit(train_data, epochs=10, validation_data=test_data)
+    print(history.history.keys())
 
-    print('hier')
-    history = model.fit(train_data, validation_data=test_data, epochs=2)
-    #history = model.fit(train_data, test_data, epochs=2)
+    #model.summary()
 
-    # for evaluation
+    loss, accuracy, recall, precision = model.evaluate(test_data)
+    print("Loss: %.2f %%" % (100*loss))
+    print("Accuracy: %.2f %%" % (100*accuracy))
+    print("Recall: %.2f %%" % (100*recall))
+    print("Precision: %.2f %%"% (100*precision))
 
     return history
-
 
 def print_metrics(test_labels, predicted_labels):
     print("Accuracy: %.2f %% " % (100.0 * accuracy_score(test_labels, predicted_labels)))
@@ -198,53 +162,38 @@ def print_metrics(test_labels, predicted_labels):
 
 
 def plot_loss_curves(history):
-    """
-    Returns separate loss curves for training and validation metrics.
-    """
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
 
-    accuracy = history.history['accuracy']
-    val_accuracy = history.history['val_accuracy']
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
 
-    recall = history.history['recall']
-    val_recall = history.history['val_recall']
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
 
-    precision = history.history['precision']
-    val_precision = history.history['val_precision']
+    plt.plot(history.history['recall'])
+    plt.plot(history.history['val_recall'])
+    plt.title('model recall')
+    plt.ylabel('recall')
+    plt.xlabel('epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
 
-    epochs = range(len(history.history['loss']))
-
-    # Plot loss
-    plt.plot(epochs, loss, label='training_loss')
-    plt.plot(epochs, val_loss, label='val_loss')
-    plt.title('Loss')
-    plt.xlabel('Epochs')
-    plt.legend()
-
-    # Plot accuracy
-    plt.figure()
-    plt.plot(epochs, accuracy, label='training_accuracy')
-    plt.plot(epochs, val_accuracy, label='val_accuracy')
-    plt.title('Accuracy')
-    plt.xlabel('Epochs')
-    plt.legend()
-
-    # Plot recall
-    plt.figure()
-    plt.plot(epochs, recall, label='training_recall')
-    plt.plot(epochs, val_recall, label='val_recall')
-    plt.title('Recall')
-    plt.xlabel('Epochs')
-    plt.legend()
-
-    # Plot precision
-    plt.figure()
-    plt.plot(epochs, precision, label='training_precision')
-    plt.plot(epochs, val_precision, label='val_precision')
-    plt.title('Precision')
-    plt.xlabel('Epochs')
-    plt.legend()
+    plt.plot(history.history['precision'])
+    plt.plot(history.history['val_precision'])
+    plt.title('model precision')
+    plt.ylabel('precision')
+    plt.xlabel('epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.show()
 
 
 def confusion_matrix_graph(test_labels, predicted_labels, labels):
@@ -253,27 +202,22 @@ def confusion_matrix_graph(test_labels, predicted_labels, labels):
     disp.plot(xticks_rotation='vertical')
     plt.show()
 
-
-# loss function graphisch
-# file-upload
-# pre-trained model
-
 def main():
     train_path = "archive/train"
     test_path = "archive/test"
 
-    # labels = load_labels(train_path)
+    labels = load_labels(train_path)
     # test_labels = load_labels(test_path)
 
     # for resizing
     # resize_images(train_path)
 
-    # train_images, train_labels = load_images_from_files(train_path, labels)
-    # test_images, test_labels = load_images_from_files(test_path, labels)
+    train_images, train_labels = load_images_from_files(train_path, labels)
+    test_images, test_labels = load_images_from_files(test_path, labels)
 
     # img = display_random_img("archive/train/", random.choice(list(train_classes)))
 
-    # np.shape(train_images), np.shape(train_labels)
+    np.shape(train_images), np.shape(train_labels)
 
     # random forest
     '''predictions = random_forest(train_images, train_labels, test_images, test_labels)
@@ -285,7 +229,8 @@ def main():
     plot_loss_curves(history)
 
     pretrained_model = pre_trained_model()
-    train_pretrained_model(pretrained_model, train_path, test_path)
+    pre_trained_history = train_pretrained_model(pretrained_model, train_path, test_path)
+    plot_loss_curves(pre_trained_history)
     return 0
 
 
@@ -296,7 +241,12 @@ num_classes = 5
 
 def pre_trained_model():
     model = get_model()
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=["accuracy", "Recall", "Precision"],
+    )
+
     model.summary()
     return model
 
@@ -329,17 +279,26 @@ def train_pretrained_model(model, train_dir, test_dir):
     valid_generator = valid_data_gen.flow_from_directory(test_dir, (image_size, image_size), batch_size=batch_size,
                                                          class_mode='categorical')
 
-    epochs = 5
-
     # Training the model
-
-    model.fit(
+    pre_trained_history = model.fit(
         train_generator,
         steps_per_epoch=train_generator.n // batch_size,
         validation_data=valid_generator,
         validation_steps=valid_generator.n // batch_size,
-        epochs=epochs,
+        epochs=10,
         verbose=1)
+
+    print(pre_trained_history.history.keys())
+
+    imagegen = ImageDataGenerator()
+    test_data = imagegen.flow_from_directory(test_dir, class_mode="categorical", batch_size=128, target_size=(256, 256), shuffle=False)
+    loss, accuracy, recall, precision = model.evaluate(test_data)
+    print("Loss: %.2f %%" % (100 * loss))
+    print("Accuracy: %.2f %%" % (100 * accuracy))
+    print("Recall: %.2f %%" % (100 * recall))
+    print("Precision: %.2f %%" % (100 * precision))
+
+    return pre_trained_history
 
 
 if __name__ == '__main__':
